45e83dcf058301ebd73ee89be9050ce5288ddccb
Romain Gayon <romaing@google.com>
2018-06-04 17:40:50 +0200

    + header

--- docker_explorer/lib/utils.py
+++ docker_explorer/lib/utils.py
@@ -1,7 +1,18 @@
-"""TODO(romaing): DO NOT SUBMIT without one-line documentation for utils.
-
-TODO(romaing): DO NOT SUBMIT without a detailed description of utils.
-"""
+# -*- coding: utf-8 -*-
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module to provide some helper methods."""
 
 from __future__ import print_function, unicode_literals
 
