3e15ae11da764843499abe8de46f272c9714eb09
Romain Gayon <romaing@google.com>
2018-06-04 14:17:28 +0200

    Move utils methods to utils.py

--- tests.py
+++ tests.py
@@ -27,10 +27,11 @@ from docker_explorer import de
 
 from docker_explorer.lib import aufs
 from docker_explorer.lib import overlay
+from docker_explorer.lib import storage
 from docker_explorer.lib import utils
 
 
-# pylint: disable=protected-access
+# pylint: disable=protected-accesso
 
 class UtilsTests(unittest.TestCase):
   """Tests Utils methods."""
