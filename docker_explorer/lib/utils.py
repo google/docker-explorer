cb4222fc36595d58078eb9029d7a2c75828c826a
Romain Gayon <romaing@google.com>
2018-06-05 13:23:18 +0200

    Move utils methods to utils.py (#20)

---
+++ docker_explorer/lib/utils.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module to provide some helper methods."""
+
+from __future__ import print_function, unicode_literals
+
+from datetime import datetime
+import json
+
+
+def FormatDatetime(timestamp):
+  """Formats a Docker timestamp.
+
+  Args:
+    timestamp (str): the Docker timestamp.
+
+  Returns:
+    str: Human readable timestamp.
+  """
+  try:
+    time = datetime.strptime(timestamp[0:26], '%Y-%m-%dT%H:%M:%S.%f')
+  except ValueError:
+    time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+  return time.isoformat()
+
+def PrettyPrintJSON(string):
+  """Generates a easy to read representation of a JSON string.
+
+  Args:
+    string (str): JSON string.
+
+  Returns:
+    str: pretty printed JSON string.
+  """
+  return json.dumps(
+      json.loads(string), sort_keys=True, indent=4, separators=(', ', ': '))
