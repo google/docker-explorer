19a63fd4ce7dcd9f79eae86269d9d0c8296bcae8
Romain Gayon <romaing@google.com>
2018-06-18 14:00:36 +0200

    raw_input is not py3 compatible

--- docker_explorer/lib/container.py
+++ docker_explorer/lib/container.py
@@ -248,7 +248,13 @@ class Container(object):
       print(c)
     print('Do you want to mount this container ID: {0:s} on {1:s} ?\n'
           '(ie: run these commands) [Y/n]'.format(self.container_id, mount_dir))
-    choice = raw_input().lower()
+    # Ugly Py2/Py3 compat code.
+    # Undo in 2020+
+    try:
+      input = raw_input  # pylint: disable=redefined-builtin
+    except NameError:
+      pass
+    choice = input().lower()
     if choice in ['y', 'yes', '']:
       for c in commands:
         # TODO() this is quite unsafe, need to properly split args
