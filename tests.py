2fa6d1707b8654252ffee92be71877c5e50ea6cb
Romain Gayon <romaing@google.com>
2018-08-03 18:27:12 -0400

    Add a couple more raising cases (fix #36) (#42)

--- tests.py
+++ tests.py
@@ -80,10 +80,7 @@ class TestDEMain(unittest.TestCase):
     de_object.docker_directory = 'this_dir_shouldnt_exist'
 
     expected_error_message = (
-        'this_dir_shouldnt_exist is not a Docker directory\n'
-        'Please specify the Docker\'s directory path.\n'
-        'hint: de.py -r /var/lib/docker')
-
+        'this_dir_shouldnt_exist is not a Docker directory')
     with self.assertRaises(errors.BadStorageException) as err:
       de_object._SetDockerDirectory('this_dir_shouldnt_exist')
     self.assertEqual(expected_error_message, err.exception.message)
