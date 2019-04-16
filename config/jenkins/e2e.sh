7598ac7898e77a419b4804699c9b37624b52d0e9
Romain Gayon <romaing@google.com>
2019-04-16 13:15:21 +0200

    cleanup code and add comments

--- config/jenkins/e2e.sh
+++ config/jenkins/e2e.sh
@@ -4,6 +4,7 @@ set -e
 
 TMP_MOUNTPOINT="$(mktemp -d)"
 
+# Installs necessary packages, as well as the code to test
 function install_packages {
   sudo apt -y install docker.io jq python-setuptools
   git clone https://github.com/google/docker-explorer || echo 'Already cloned'
@@ -11,21 +12,27 @@ function install_packages {
   sudo python setup.py install
 }
 
+# Checks a string against another, fails if they don't match.
+# Args:
+#   A message for the current text, as string,
+#   The expected result, as string,
+#   The result to check.
 function assert_equal {
   local message="${1}"
   local expected="${2}"
   local result="${3}"
 
   echo "${message}"
-  if [[ "${expected}" == "${result}" ]]; then
-      echo "'${expected}' == '${result}'"
-      return 0
-  else
-      echo "'${expected}' != '${result}'"
+  if [[ "${expected}" != "${result}" ]]; then
+      echo "Expected:"
+      echo "${expected}"
+      echo "Got:"
+      echo "${result}"
       return 1
   fi
 }
 
+# Cleans up the environment
 function cleanup {
   umount "${TMP_MOUNTPOINT}" || true
   rm -rf "${TMP_MOUNTPOINT}"
@@ -38,17 +45,35 @@ function cleanup {
   rm -rf /var/lib/docker
 }
 
-function setup_docker {
+# Starts a detached container
+function start_docker {
     docker run -d wordpress:latest
 }
 
-function run_de {
+# Tests that docker-explorer detects one running container
+function test_containers_nb {
   local result
   result="$(de.py -r /var/lib/docker list running_containers | jq '. | length' )"
   assert_equal "Number of containers should be 1" "1" "${result}"
+  return 0
+}
 
+# Tests that docker-explorer detects the container name
+function test_container_name {
+  local result
   result="$(de.py -r /var/lib/docker list running_containers | jq '.[0]["image_name"]' )"
-  assert_equal "First container name should be wordpress:latest" "\"wordpress:latest\"" $result
+  assert_equal "First container name should be wordpress:latest" \
+    "\"wordpress:latest\"" $result
+  return 0
+}
+
+# Checks that docker-explorer can mount a container filesystem and find
+# relevant files
+function test_container_mount {
+  local container_id
+  local fake_bad_path
+  local fake_bad_content
+  local content
 
   container_id="$(de.py -r /var/lib/docker list running_containers | jq '.[0]["container_id"]'  | cut -d '"' -f 2)"
   echo "Trying to mount container ID ${container_id} on ${TMP_MOUNTPOINT}"
@@ -56,7 +81,7 @@ function run_de {
 
   fake_bad_path="/tmp/definitely_bad"
   fake_bad_content="NOT A MALWARE"
-  echo "Adding 'bad file' in ${fake_bad_path}"
+  echo "Adding bad file in ${fake_bad_path}"
   docker exec -i -t "${container_id}" sh -c "echo '${fake_bad_content}' > '${fake_bad_path}'"
   path_to_file="${TMP_MOUNTPOINT}/${fake_bad_path}"
   if [[ -f "${path_to_file}" ]]; then
@@ -66,12 +91,21 @@ function run_de {
     echo "Didn't find expected bad file at ${path_to_file}"
     return 1
   fi
+  return 0
+}
+
+# Runs docker-explorer tests
+function run_de_tests {
+  test_containers_nb
+  test_container_name
+  test_container_mount
   echo "ALL TESTS PASS!"
+  return 0
 }
 
 function main {
   install_packages
-  setup_docker
+  start_docker
   run_de
   cleanup
 }
